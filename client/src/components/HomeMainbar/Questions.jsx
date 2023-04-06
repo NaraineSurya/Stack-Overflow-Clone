ccb7e91a7bf4107d093990ca9d50275a56e566a4
NaraineSurya <narayanansuryatcr@gmail.com>
2023-04-06 07:31:22 +0530

    homemainbar

--- client/src/components/HomeMainbar/Questions.jsx
+++ client/src/components/HomeMainbar/Questions.jsx
@@ -2,34 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Questions = ({question}) => {
-  // return (
-  //   <div className='display-ans-container'>
-  //     <div className="display-votes-ans">
-  //       <p>{question.votes}</p>
-  //       <p>votes</p>
-  //     </div>
-  //     <div className="display-votes-ans">
-  //       <p>{question.noofAnswers}</p>
-  //       <p>Answers</p>
-  //     </div>
-  //     <div className="display-question-details">
-  //       <Link to={`/Questions/${question.id}`} className='question-title-link'>{question.qustionTitle}</Link>
-  //       <div className="display-tags-time">
-  //         <div className="display-tags">
-  //           {
-  //             question.questionTags.map((tag) => {
-  //               <p key={tag}>{tag}</p>
-  //             })
-  //           }
-  //         </div>
-  //         <p className='disply-time'>
-  //           asked {question.askedOn} {question.UserPosted}
-  //         </p>
-  //       </div>
-  //     </div>
+  return (
+    <div className='display-question-container'>
+      <div className="display-votes-ans">
+        <p>{question.votes}</p>
+        <p>votes</p>
+      </div>
+      <div className="display-votes-ans">
+        <p>{question.noOfAnswers}</p>
+        <p>Answers</p>
+      </div>
+      <div className="display-question-details">
+        <Link to={`/Questions/${question.id}`} className='question-title-link'>{question.questionTitle}</Link>
+        <div className="display-tags-time">
+          <div className="display-tags">
+            {
+              question.QuestionTags.map((tag) => (
+                  <p key={tag}>{tag}</p>
+              )) 
+            }
+          </div>
+          <p className='display-time'></p>
+          asked {question.askedOn} {question.userPosted}
+        </div>
+      </div>
     
-  //   </div>
-  // )
+    </div>
+  )
 }
 
 export default Questions
